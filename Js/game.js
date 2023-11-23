c12ce38aad3a2177a043feb79c667257654a1fc4
Nima Reza <NimaaRezaa99@gmail.com>
2023-11-23 15:37:53 +0330

    next question, correct answer and score calculator completed

--- Js/game.js
+++ Js/game.js
@@ -3,13 +3,25 @@
 import formatData from "./formatter.js";
 
 
-// !Variables & Selectors
+// !Selectors & Variables
 
 const loader = document.getElementById("loader");
 const containerBox = document.getElementById("container");
+const questionBox = document.getElementById("question");
+const nextBtn = document.getElementById("next-question");
+const answerList = document.querySelectorAll(".answer--text");
+const scoreCalculator = document.getElementById("your--score_span");
+const questionNo = document.getElementById("question--number_span")
 
+
+const CORRECT_BONUS = 10;
 const URL = "https://opentdb.com/api.php?amount=15&difficulty=easy&type=multiple";
+
 let formattedData = null;
+let questionIndex = 0;
+let correctAnswer = null;
+let score = 0;
+let isAccepted = true;
 
 // !Functions
 
@@ -18,17 +30,68 @@ const fetchData = async () => {
     const response = await fetch(URL);
     const json = await response.json();
     formattedData = formatData(json.results);
-    console.log (formattedData);
     start();
 
 }
-
 const start = () => {
+
+    questionTextUpdater()
     loader.style.display = "none";
     containerBox.style.display = "block";
 }
+
+const questionTextUpdater = () => {
+    
+    const {question, answers, correctAnswerIndex} = 
+    formattedData[questionIndex];
+    questionNo.innerText = questionIndex+1;
+    correctAnswer = correctAnswerIndex;
+    questionBox.innerText = question;
+    answerList.forEach((item, index) => {
+        item.innerText = answers[index];
+    })
+
+}
+
+const checkAnswer = (event, index) => {
+
+    if(!isAccepted) return;
+    isAccepted = false;
+
+    const isCorrect = index === correctAnswer ? true : false;
+    if ( isCorrect ) {
+        event.target.classList.add("correct");
+        score += CORRECT_BONUS;
+        scoreCalculator.innerText = score;
+    } else { 
+        event.target.classList.add("incorrect");
+        answerList[correctAnswer].classList.add("correct");
+    }
+}
+
+const nextQuestionHandler = () => {
+    
+    if ( questionIndex < formattedData.length - 1) {
+        questionIndex++;
+        isAccepted = true;
+        removeClassHandler();
+        questionTextUpdater();
+    } else {
+        window.location.assign("/end.html");
+    }
+}
+
+const removeClassHandler = () => {
+    answerList.forEach(button => button.classList="answer--text")
+}
+
+
 
 
 // !EventListeners
 
 window.addEventListener("load",fetchData);
+answerList.forEach((button, index) => {
+    button.addEventListener("click", (event) => checkAnswer(event, index))
+});
+nextBtn.addEventListener("click", nextQuestionHandler)
