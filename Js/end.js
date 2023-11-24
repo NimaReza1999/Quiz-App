c25815d6a1e9fa7b477399e2709d94cbed73a00b
Nima Reza <NimaaRezaa99@gmail.com>
2023-11-24 11:45:18 +0330

    highScores in localstorage created

--- Js/end.js
+++ Js/end.js
@@ -5,6 +5,7 @@ const score = JSON.parse(localStorage.getItem("score"));
 const scoreEle = document.getElementById("score");
 const saveBtn = document.getElementById("save-btn");
 const nameInput = document.getElementById("name-input");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 scoreEle.innerText = score;
 
@@ -18,8 +19,21 @@ const backHandler = () => {
 
 const saveHandler = () => {
 
+    if (!nameInput.value || !score) {
+        alert("Invalid Name or Score");
+    } else {
     const playerName = nameInput.value;
-    console.log(playerName);
+    const finalScore = {
+        name: playerName,
+        score,
+    }
+    highScores.push(finalScore);
+    highScores.sort ((a, b) => b.score - a.score);
+    highScores.splice(10);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    localStorage.removeItem("Score");
+    window.location.assign("/");   
+}
 }
 
 
